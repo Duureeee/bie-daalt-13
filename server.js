fbb62fe98736dcd2a63b1665992b6a911ec09d46
Duureeee <Dbileg04@gmail.com>
2026-05-06 00:17:55 +0800

    server.js

--- server.js
+++ server.js
@@ -6,9 +6,57 @@ const PORT = 3000;
 // JSON request body унших тохиргоо
 app.use(express.json());
 
+// Түр хадгалах массив
+// Дараа нь database эсвэл JSON file болгож сайжруулж болно
+let urls = [];
+
+// Санамсаргүй short code үүсгэх функц
+function generateShortCode() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  let code = "";
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    code += characters[randomIndex];
+  }
+
+  return code;
+}
+
 // Энгийн test route
 app.get("/", (req, res) => {
   res.send("URL Shortener backend is running");
+});
+
+// Урт URL-оос богино URL үүсгэх endpoint
+app.post("/api/shorten", (req, res) => {
+  const { longUrl, expiresAt } = req.body;
+
+  if (!longUrl) {
+    return res.status(400).json({
+      error: "longUrl is required",
+    });
+  }
+
+  const shortCode = generateShortCode();
+
+  const newUrl = {
+    longUrl: longUrl,
+    shortCode: shortCode,
+    shortUrl: `http://localhost:${PORT}/${shortCode}`,
+    clickCount: 0,
+    createdAt: new Date().toISOString(),
+    expiresAt: expiresAt || null,
+  };
+
+  urls.push(newUrl);
+
+  res.status(201).json({
+    shortCode: newUrl.shortCode,
+    shortUrl: newUrl.shortUrl,
+  });
 });
 
 // Server асаах хэсэг
